810b8b31473621541540e0d4d8345f4ee74d16b0
Kaitlyn Delmain <kaydelmain@gmail.com>
2022-07-16 23:01:45 -0500

    Add project files.

---
+++ Blazor Checkers/Client/Data/Checker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkers.Client.Data
+{
+    public class Checker
+    {
+        public int boardRow { get; set; }
+        public int boardColumn { get; set; }
+        public CheckerDirection direction { get; set; }
+        public string checkerColor { get; set; }
+    }
+
+    public enum CheckerDirection
+    {
+        Down, Up, Both
+    }
+}
